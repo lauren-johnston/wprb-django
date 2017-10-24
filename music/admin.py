93165b781c962ba3b5883129a4165b2851001ec6
Rob Whitaker <rmw2@princeton.edu>
2017-10-24 12:07:23 -0400

    Registered models with Django Admin

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Artist, Song, Album, Label, Genre, Subgenre
+
 # Register your models here.
+admin.site.register(Artist)
+admin.site.register(Song)
+admin.site.register(Album)
+admin.site.register(Label)
+admin.site.register(Genre)
+admin.site.register(Subgenre)
--- playlist/admin.py
+++ playlist/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Spin, Playlist, DJ, Show, Settings
+
 # Register your models here.
+admin.site.register(Spin)
+admin.site.register(Playlist)
+admin.site.register(DJ)
+admin.site.register(Show)
+admin.site.register(Settings)
