4e0dba4a83c8a7bbbd67f825f48e99f418f7a7e8
Rob Whitaker <rmw2@princeton.edu>
2017-11-08 10:52:01 -0500

    added comment urls to playlist urlconf

--- playlist/urls.py
+++ playlist/urls.py
@@ -9,10 +9,14 @@ and editing
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from . import views
+from . import views, services
 
 playlist_id = r'^(?P<playlist_id>[0-9]+)/'
 
 urlpatterns = [
 	url(playlist_id + '$', views.edit_playlist, name='edit-playlist'),
+	url(playlist_id + 'comment/new', services.comment.new, name='comment-new'),
+	url(playlist_id + 'comment/edit', services.comment.edit, name='comment-edit'),
+	url(playlist_id + 'comment/new', services.comment.new, name='comment-new'),
+	url(playlist_id + 'comment/delete', services.comment.delete, name='comment-delete'),
 ]
