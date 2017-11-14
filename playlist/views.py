ddc737db15ef6b4c3e996ee6270e8d64389312b2
Rob Whitaker <rmw2@princeton.edu>
2017-11-14 12:07:07 -0500

    Added commented playlist finding logic to views.py

--- playlist/views.py
+++ playlist/views.py
@@ -7,12 +7,6 @@ def edit_playlist(request, playlist_id):
 	""" Serve the page for editing a single playlist.
 	"""
 
-	spins = [{
-		'title'	: 'Track %d' % i,
-		'artist': ['Artist %d' % i],
-		'album'	: 'Album %d' % i
-	} for i in range(10)]
-
 	show = {
 		'title'  	: 'A Cool Show',
 		'subtitle'	: 'with a subtitle: ' + playlist_id,
@@ -21,5 +15,20 @@ def edit_playlist(request, playlist_id):
 		'subgenre'	: ['A mike genre', 'shit-slop funk bass', 'fruit loops'],
 		'desc'		: 'Here you can talk about this episode of your show. Anything in particular you want to say?'
 	}
+
+	# playlist = Playlist.objects.get(pk=playlist_id)
+
+	# spins = [{
+	# 	'title'	: spin.song.title
+	# 	'artist': [a.name for a in spin.song.artist],
+	# 	'album'	: spin.song.album.name
+	# } for spin in playlist.spin_set]
+
+	spins = [{
+		'title'	: 'Track %d' % i,
+		'artist': ['Artist %d' % i],
+		'album'	: 'Album %d' % i
+	} for i in range(10)]
+
 
 	return render(request, "edit.html", context={'spins': spins, 'show': show})
