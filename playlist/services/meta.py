42e85c92ffe095cdb8998ae1ba55787673ffe669
Rob Whitaker <rmw2@princeton.edu>
2017-11-13 20:16:16 -0500

    (style) changed docstrings to use double quotes

--- playlist/services/meta.py
+++ playlist/services/meta.py
@@ -1,6 +1,6 @@
-''' meta.py
+""" meta.py
 	Implements server side API for meta-playlist operations.
-'''
+"""
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from django.http import QueryDict, HttpResponse, JsonResponse
@@ -12,9 +12,9 @@ from ..common import *
 @login_required
 @require_http_methods(["PUT"])
 def subtitle(request, playlist_id):
-	''' Request contains 'title' and uri contains playlist_id
+	""" Request contains 'title' and uri contains playlist_id
 	    Sets the title of playlist_id to 'title'
-	'''
+	"""
 	try: 
 		args = QueryDict(request.body)
 		playlist = Playlist.objects.get(pk=playlist_id)
@@ -30,9 +30,9 @@ def subtitle(request, playlist_id):
 @login_required
 @require_http_methods(["PUT"])
 def desc(request, playlist_id):
-	''' Request contains 'desc' and uri contains playlist_id
+	""" Request contains 'desc' and uri contains playlist_id
 	    Sets the desc of playlist_id to 'desc'
-	'''
+	"""
 	try: 
 		args = QueryDict(request.body)
 		playlist = Playlist.objects.get(pk=playlist_id)
@@ -48,14 +48,14 @@ def desc(request, playlist_id):
 @login_required
 @require_http_methods(["PUT"])
 def genre(request, playlist_id):
-	''' Request contains 'genre' and uri contains playlist_id
+	""" Request contains 'genre' and uri contains playlist_id
 	    Sets the genre of playlist_id to 'genre'
-	'''
+	"""
 	try:
 		args = QueryDict(request.body)
 		playlist = Playlist.objects.get(pk=playlist_id)
-		genre    = Genre.objects.get(name__iexact=args['genre'])
-		playlist.genres.add(genre)
+		genre = Genre.objects.get(name__iexact=args['genre'])
+		playlist.genre_set.add(genre)
 	# If no playlist...
 	except Playlist.DoesNotExist:
 		return error('Invalid playlist id')
@@ -64,9 +64,7 @@ def genre(request, playlist_id):
 		return error('Query does not contain required arguments')
 	# If no genre match...
 	except Genre.DoesNotExist:
-		new_genre = Genre(name=put['genre'])
-		new_genre.save()
-		playlist.genres.add(new_genre)
+		playlist.genre_set.create(name=args['genre'])
 
 	playlist.save()
 	return success()
@@ -74,10 +72,10 @@ def genre(request, playlist_id):
 @login_required
 @require_http_methods(["POST"])
 def add_subgenre(request, playlist_id):
-	''' Request contains 'subgenre' and uri contains playlist_id
+	""" Request contains 'subgenre' and uri contains playlist_id
 	    POST: Adds the subgenre 'subgenre' to playlist_id 
 	    DELETE: Removes the subgenre 'subgenre' from playlist_id
-	'''
+	"""
 	try: 
 		args     = QueryDict(request.body)
 		playlist = Playlist.objects.get(pk=playlist_id)
@@ -87,12 +85,12 @@ def add_subgenre(request, playlist_id):
 	except KeyError:
 		return error('Query does not contain required arguments')
 	except Subgenre.DoesNotExist:
-		playlist.subgenres.create(name=args['subgenre'], author=request.user)
+		playlist.subgenre_set.create(name=args['subgenre'], author=request.user)
 		playlist.save()
 		return success()
 
 	if not (playlist.subgenre_set.filter(pk=subgenre.pk).exists()):
-		playlist.subgenres.add(subgenre)
+		playlist.subgenre_set.add(subgenre)
 		playlist.save()
 
 	return success()
@@ -100,10 +98,10 @@ def add_subgenre(request, playlist_id):
 @login_required
 @require_http_methods(["DELETE"])
 def del_subgenre(request, playlist_id):
-	''' Request contains 'subgenre' and uri contains playlist_id
+	""" Request contains 'subgenre' and uri contains playlist_id
 	    POST: Adds the subgenre 'subgenre' to playlist_id 
 	    DELETE: Removes the subgenre 'subgenre' from playlist_id
-	'''
+	"""
 	try: 
 		args     = QueryDict(request.body)
 		playlist = Playlist.objects.get(pk=playlist_id)
@@ -112,7 +110,7 @@ def del_subgenre(request, playlist_id):
 		return error('Invalid playlist id')
 	except KeyError:
 		return error('Query does not contain required arguments')
-	except Subgenre.DoesNotExist:
+	except Subgenre.DoesNotExist: 
 		return success()
 
 	matching_contents = playlist.subgenre_set.filter(pk=subgenre.pk)
