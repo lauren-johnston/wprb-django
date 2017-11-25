2f063718a3fcad06eda81c4dc38e08b6ab805a42
Rob Whitaker <rmw2@princeton.edu>
2017-11-25 15:06:33 -0500

    Configured node packages and webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require("path")
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = {
+  context: __dirname,
+
+  // where to find the files that we want! 
+  // entry means "entry-point"
+  entry: {
+    // bundle name: location
+    'playlist': './assets/playlist/js/playlist-entry',
+  },
+
+  output: {
+      // Where to put the compiled bundles
+      path: path.resolve('./assets/bundles/'),
+      // What to name the compiled bundles
+      filename: "[name]-[hash].js",
+  },
+
+  plugins: [
+    // Output status of webpack, communicates with django-webpack-loader
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+
+  module: {
+    // configuration for the javascript compilation
+    loaders: [
+      { 
+        test: /\.jsx?$/, 
+        exclude: /node_modules/,
+        // babel does the actual compilation
+        loader: 'babel-loader', 
+        query: {
+            // env gives us all modern syntax, react gives JSX
+            presets: ['env', 'react']
+        },
+      }
+    ],
+  },
+
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx']
+  },
+}
