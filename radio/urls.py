cbb03c728119c41308a57b5e35b78f0205a1f638
Rob Whitaker <rmw2@princeton.edu>
2017-11-29 09:42:38 -0500

    added redirect from / to /playlist

--- radio/urls.py
+++ radio/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/playlist/')),
     url(r'^admin/', admin.site.urls),
     url(r'^playlist/', include('playlist.urls')),
     url(r'^explore/', include('playlist.urls')),
