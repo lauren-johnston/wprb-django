bc40d108f4f5a18bc9701e021ace278d5e310958
Lauren Johnston <ljohnston@princeton.edu>
2018-08-11 10:56:58 -0400

    added BlogPage model to wag app

--- wag/models.py
+++ wag/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from django.contrib.auth.models import User
 from playlist.models import Spin, Playlist, DJ, Show, Settings, Comment
+from modelcluster.fields import ParentalKey
+from wagtail.search import index
 
 class DJPage(Page):
     body = RichTextField()
     date = models.DateField("Post date")
-    spin = Spin()
+    dj = models.OneToOneField(
+        'playlist.DJ',
+         null = True,
+        on_delete=models.SET_NULL,
+
+    )
     feed_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -32,3 +39,57 @@ class DJPage(Page):
 
     def get_template(self, request):
         return 'dj_page.html'
+
+
+class BlogPage(Page):
+
+    # Database fields
+
+    body = RichTextField()
+    date = models.DateField("Post date")
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+
+    # Search index configuration
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+        index.FilterField('date'),
+    ]
+
+
+    # Editor panels configuration
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('body', classname="full"),
+        InlinePanel('related_links', label="Related links"),
+    ]
+
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+        ImageChooserPanel('feed_image'),
+    ]
+
+
+    # Parent page / subpage type rules
+
+    parent_page_types = ['blog.BlogIndex']
+    subpage_types = []
+
+
+class BlogPageRelatedLink(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='related_links')
+    name = models.CharField(max_length=255)
+    url = models.URLField()
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('url'),
+    ]
